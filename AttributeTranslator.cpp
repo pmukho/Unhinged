00fb62fb6a193738a582c2a57e825eb50dc0c8b3
Pritam <pmukho2003@gmail.com>
2022-03-06 02:07:51 -0800

    declared AbstractTranslator

--- AttributeTranslator.cpp
+++ AttributeTranslator.cpp
@@ -1,2 +1,24 @@
 // AttributeTranslator.cpp
 
+#include "AttributeTranslator.h"
+
+AttributeTranslator::AttributeTranslator()
+	: m_rtreeAttValPair(new RadixTree<AttValPair>)
+{}
+
+AttributeTranslator::~AttributeTranslator()
+{
+	delete m_rtreeAttValPair;
+}
+
+bool AttributeTranslator::Load(std::string filename)
+{
+	return true;
+}
+
+std::vector<AttValPair> AttributeTranslator::FindCompatibleAttValPairs(const AttValPair& source) const
+{
+	std::vector<AttValPair> v;
+	return v;
+}
+
--- AttributeTranslator.h
+++ AttributeTranslator.h
@@ -3,6 +3,19 @@
 #ifndef ATTRIBUTETRANSLATOR_INCLUDED
 #define ATTRIBUTETRANSLATOR_INCLUDED
 
+#include <vector>
+#include <string>
+#include "provided.h"
+#include "RadixTree.h"
 
+class AttributeTranslator {
+public:
+	AttributeTranslator();
+	~AttributeTranslator();
+	bool Load(std::string filename);
+	std::vector<AttValPair> FindCompatibleAttValPairs(const AttValPair& source) const;
+private:
+	RadixTree<AttValPair>* m_rtreeAttValPair;
+};
 
 #endif // ATTRIBUTETRANSLATOR_INCLUDED
