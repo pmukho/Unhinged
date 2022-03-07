4786fb9a30eb964f68efcbc82214a7c944b4a6e1
Pritam <pmukho2003@gmail.com>
2022-03-07 15:57:28 -0800

    tried implementing AttributeTranslator with an unordered set

--- AttributeTranslator.cpp
+++ AttributeTranslator.cpp
@@ -4,12 +4,13 @@
 #include <fstream>
 
 AttributeTranslator::AttributeTranslator()
-	: m_rtreeAttValPair(new RadixTree<AttValPair>)
+	//: m_rtreeAttSValSPairCompatible(new RadixTree<RadixTree<std::vector<AttValPair*>>>)
+	: m_rtreeAttSValSPairCompatible(new RadixTree<RadixTree<std::unordered_set<AttValPair>>>)
 {}
 
 AttributeTranslator::~AttributeTranslator()
 {
-	delete m_rtreeAttValPair;
+	delete m_rtreeAttSValSPairCompatible;
 }
 
 bool AttributeTranslator::Load(std::string filename)
@@ -20,12 +21,33 @@ bool AttributeTranslator::Load(std::string filename)
 	}
 
 	if (translatorFile.is_open()) {
-		std::string attS, valS, attC, valC, skip;
+		std::string attSRead, valSRead, attCRead, valCRead, skip;
 		while (translatorFile.good()) {
-			std::getline(translatorFile, attS, ',');
-			std::getline(translatorFile, valS, ','); 
-			std::getline(translatorFile, attC, ',');
-			std::getline(translatorFile, valC);
+			std::getline(translatorFile, attSRead, ',');
+			std::getline(translatorFile, valSRead, ','); 
+			std::getline(translatorFile, attCRead, ',');
+			std::getline(translatorFile, valCRead);
+
+			//RadixTree<std::vector<AttValPair*>>* attSBranch = m_rtreeAttSValSPairCompatible->search(attSRead);
+			RadixTree<std::unordered_set<AttValPair>>* attSBranch = m_rtreeAttSValSPairCompatible->search(attSRead);
+			if (attSBranch == nullptr) { // unknown attribute, start new attSBranch
+				//m_rtreeAttSValSPairCompatible->insert(attSRead, RadixTree<std::vector<AttValPair*>>());
+				m_rtreeAttSValSPairCompatible->insert(attSRead, RadixTree<std::unordered_set<AttValPair>>());
+				attSBranch = m_rtreeAttSValSPairCompatible->search(attSRead);
+			}
+			//on attSBranch now
+			//std::vector<AttValPair*>* valSBranch = attSBranch->search(valSRead);
+			std::unordered_set<AttValPair>* valSBranch = attSBranch->search(valSRead);
+			if (valSBranch == nullptr) { // unknown val, start new valSBranch
+				//attSBranch->insert(valSRead, std::vector<AttValPair*>());
+				attSBranch->insert(valSRead, std::unordered_set<AttValPair>());
+				valSBranch = attSBranch->search(valSRead);
+			}
+			//on valSBranch now
+			valSBranch->insert(AttValPair(attCRead, valCRead));
+			//need to update both trees
+			//attSBranch->insert(valSRead, *valSBranch);
+			//m_rtreeAttSValSPairCompatible->insert(attSRead, *attSBranch);
 		}
 	}
 	return true;
@@ -34,6 +56,13 @@ bool AttributeTranslator::Load(std::string filename)
 std::vector<AttValPair> AttributeTranslator::FindCompatibleAttValPairs(const AttValPair& source) const
 {
 	std::vector<AttValPair> v;
+	RadixTree<std::unordered_set<AttValPair>>* attSBranch = m_rtreeAttSValSPairCompatible->search(source.attribute);
+	if (attSBranch != nullptr) {
+		std::unordered_set<AttValPair>* valSBranch = attSBranch->search(source.value);
+		if (valSBranch != nullptr) {
+			v = std::vector<AttValPair>(valSBranch->begin(), valSBranch->end());
+		}
+	}
 	return v;
 }
 
--- AttributeTranslator.h
+++ AttributeTranslator.h
@@ -4,6 +4,7 @@
 #define ATTRIBUTETRANSLATOR_INCLUDED
 
 #include <vector>
+#include <unordered_set>
 #include <string>
 #include "provided.h"
 #include "RadixTree.h"
@@ -16,7 +17,8 @@ public:
 	std::vector<AttValPair> FindCompatibleAttValPairs(const AttValPair& source) const;
 private:
 	//outer tree to search val for given att, inner tree to serach compatible AttValPair given value associated with att
-	RadixTree<RadixTree<std::vector<AttValPair*>*>*>* m_rtreeAttSValSPairCompatible;
+	//RadixTree<RadixTree<std::vector<AttValPair*>>>* m_rtreeAttSValSPairCompatible;
+	RadixTree<RadixTree<std::unordered_set<AttValPair>>>* m_rtreeAttSValSPairCompatible;
 };
 
 #endif // ATTRIBUTETRANSLATOR_INCLUDED
