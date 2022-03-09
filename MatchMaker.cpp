a076fa9a0d4ae6ce154ddb01d3b2371d6926a053
Pritam <pmukho2003@gmail.com>
2022-03-08 19:04:39 -0800

    MatchMaker declarations

--- MatchMaker.cpp
+++ MatchMaker.cpp
@@ -1,2 +1,18 @@
 // MatchMaker.cpp
 
+#include "MatchMaker.h"
+
+MatchMaker::MatchMaker(const MemberDatabase& mdb, const AttributeTranslator& at)
+	: m_mdb(new MemberDatabase(mdb)), m_at(new AttributeTranslator(at))
+{}
+
+MatchMaker::~MatchMaker()
+{
+	delete m_mdb;
+	delete m_at;
+}
+
+std::vector<EmailCount> MatchMaker::IdenitfyRankedMatches(std::string email, int threshold) const
+{
+	return std::vector<EmailCount>();
+}
--- MatchMaker.h
+++ MatchMaker.h
@@ -3,4 +3,17 @@
 #ifndef MATCHMAKER_INCLUDED
 #define MATCHMAKER_INCLUDED
 
+#include "MemberDatabase.h"
+#include "AttributeTranslator.h"
+
+class MatchMaker {
+public:
+	MatchMaker(const MemberDatabase& mdb, const AttributeTranslator& at);
+	~MatchMaker();
+	std::vector<EmailCount> IdenitfyRankedMatches(std::string email, int threshold) const;
+private:
+	MemberDatabase* m_mdb;
+	AttributeTranslator* m_at;
+};
+
 #endif // MATCHMAKER_INCLUDED
